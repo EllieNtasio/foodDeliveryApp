93a1106e09ca56fb56e42f5b43162272e43f00ab
EllieNtasio <ellintasio@gmail.com>
2025-03-28 11:48:37 +0200

    Header ready

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,7 +4,11 @@ import './Header.css'
 const Header = () => {
   return (
     <div className='header'>
-      
+      <div className="header-contents"> 
+        <h2>Order what you need here!</h2>
+        <p>Craving something delicious? Get your favorite meals delivered fast from the best local restaurants. Browse, order, and enjoy – all with just a few taps. Your next great meal is just minutes away!</p>
+         <button>View Menu</button>
+      </div>
     </div>
   )
 }
